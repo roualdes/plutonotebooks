0b37d7971bd17caddc2a0832a79e4433294261eb
Edward A. Roualdes <eroualdes@csuchico.edu>
2021-04-02 14:52:41 -0700

    start set theory

---
+++ notebooks/set_theory.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7b6c475a-93fd-11eb-1a9e-9fe63c51edef
+begin
+	using PlutoUI
+end;
+
+# ╔═╡ 62ce1e78-93fd-11eb-2d07-f908d0883cce
+md"# Set Theory"
+
+# ╔═╡ 925c0fe2-93fd-11eb-18b8-673cd343b185
+TableOfContents()
+
+# ╔═╡ a6b9e0f4-93fd-11eb-1e20-4bac091b7066
+md"## Sets"
+
+# ╔═╡ 99665cb6-93fd-11eb-25d6-43e5a9cce8a8
+md"## Operations"
+
+# ╔═╡ a5109662-93fd-11eb-3621-01d059d93a9e
+
+
+# ╔═╡ Cell order:
+# ╠═62ce1e78-93fd-11eb-2d07-f908d0883cce
+# ╠═925c0fe2-93fd-11eb-18b8-673cd343b185
+# ╠═a6b9e0f4-93fd-11eb-1e20-4bac091b7066
+# ╠═99665cb6-93fd-11eb-25d6-43e5a9cce8a8
+# ╠═a5109662-93fd-11eb-3621-01d059d93a9e
+# ╠═7b6c475a-93fd-11eb-1a9e-9fe63c51edef
