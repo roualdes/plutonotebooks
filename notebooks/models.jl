7d09f0f85ab75443dd1b5719ac99b6d2c2fa23a1
Edward A. Roualdes <eroualdes@csuchico.edu>
2021-04-24 01:02:39 -0700

    start future 350 notes

---
+++ notebooks/models.jl
@@ -0,0 +1,27 @@
+### A Pluto.jl notebook ###
+# v0.14.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 68e943ec-a4d2-11eb-0d1b-05f66fffcb06
+md"# Statistical Models"
+
+# ╔═╡ 942948a3-584f-4e5d-a17a-5608cf154edc
+md"Mostly a discussion about density functions, but density functions are not obviously composable, so I try to frame the discussion around thinking about how data may have been generated."
+
+# ╔═╡ b2d885e9-b7de-40d6-b390-9555448e93f7
+md"We *assume* data are produced by such models, but we must emphasize this assumption lest we get carried away and start to think that our models are real."
+
+# ╔═╡ e80efa62-fb0f-4ae6-891e-c6d4ea3c0bd7
+
+
+# ╔═╡ 40e6bcb7-5add-4bd0-bb01-7e58d79a24df
+
+
+# ╔═╡ Cell order:
+# ╠═68e943ec-a4d2-11eb-0d1b-05f66fffcb06
+# ╠═942948a3-584f-4e5d-a17a-5608cf154edc
+# ╠═b2d885e9-b7de-40d6-b390-9555448e93f7
+# ╠═e80efa62-fb0f-4ae6-891e-c6d4ea3c0bd7
+# ╠═40e6bcb7-5add-4bd0-bb01-7e58d79a24df
