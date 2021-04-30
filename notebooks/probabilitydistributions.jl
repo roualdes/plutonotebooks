aeb005a36f86f2785125fb5112a7761941a4959a
Edward A. Roualdes <eroualdes@csuchico.edu>
2021-04-30 11:50:21 -0700

    redoing without expectation

--- notebooks/probabilitydistributions.jl
+++ notebooks/probabilitydistributions.jl
@@ -102,26 +102,30 @@ md"Since the ``A_n`` are disjoint, we can apply the third axiom of probability t
 $$\mathbb{P}[A] = \mathbb{P}[\cup_{n \in \{1, 2, 3, 4, 5\}} A_n] = \mathbb{P}[A_1] + \mathbb{P}[A_2] + \mathbb{P}[A_3] + \mathbb{P}[A_4] + \mathbb{P}[A_5] = \sum_{n \in \{1, 2, 3, 4, 5\}} \mathbb{P}[A_n].$$"
 
 # ╔═╡ 59588307-aaa6-45ab-862b-759b9dfc6810
-md"## Probability as Area Under Functions"
+md"## Generalizing Probability"
 
-# ╔═╡ edc9101c-af3b-40f6-b8f3-cf1570f70b9b
-md"The most common way to represent probability, from a particular probability distribution, is as area under the distribution's density function.  A probability distribution's density function is a topic unto itself.  For futher details about these functions you should read the notebook Density Functions.
+# ╔═╡ a91d6013-be1a-406d-a193-906e0c8d5a8c
+md"Probability distributions dictate the assignment of probability to arbitrary subsets of the sample space.  Only in the simplest of cases do probability calculations become calculations of the number of elements in the subset ``A`` relative to the number of elements in an enclosing space, which is often but not always the sample space ``S``, such as
 
-TODO how to link across notebooks? Need to consider different environments."
+$$\mathbb{P}[A] = |A| / |S|.$$
 
-# ╔═╡ e2268550-0ffe-47c2-a23a-e0fa220e0ddb
-md"After about calculus 2, when you hear the phrase area under a function we immediately think integral and maybe a picture like this, and this is exactly what you should have in mind."
+For instance, this definition immediately won't apply to uncountable sample spaces."
 
-# ╔═╡ 01fef914-3f03-43fa-aaf2-d9efafeb9c7b
-begin
-	f(x) = 0.0 < x < 1.0 ? x^2 : 0.0
-	x = -1:0.01:2
-	xfill = 0.5:0.01:0.75
-	cols = get_color_palette(:auto, 17)
-	plot(x, f.(x), label = false, c = cols[1], xlimits = (-1, 2),
-		xlabel = L"x", ylabel = L"f(x)")
-	plot!(xfill, xfill.^2, linecolor = false, fill = (0, cols[1]), label = false)
-end
+# ╔═╡ 272ce517-d9ec-4afe-b5c1-90d6c4c89899
+md"In general, probability is defined to be area under a probability distribution's  density function.  See the notebook on [TODO add link to] Density Functions for more details.  For now, we will assume that a density function of a probability distribution is the function that enables the assignment of probability to subsets of the sample space."
+
+# ╔═╡ f10cecb7-cb04-43ee-8db0-f623d99115c1
+md"Luckily, defining probability as area under a density function conforms to 
+
+$$\mathbb{P}[A] = |A| / |S|$$
+
+whent ``\mathbb{P}`` represents a uniform distribution (of probability) over a finite sample space ``S``, and enables more complex definitions of probability.  Let's start with the fair die example from above."
+
+# ╔═╡ 26cdcadc-f212-4cd2-a159-c85fc1a6574f
+md"The plot below represents the density function for a fair die, which we will see in Section Examples is written in statistics notation as ``\text{Uniform}(1, 6)``."
+
+# ╔═╡ 77a6274a-e7d3-4700-a404-e4c32ec8a29e
+repeat([0, 1/6], 3, 2)
 
 # ╔═╡ 6f0da015-f70a-490c-b116-9b9204371f76
 md"We use this canonical image of area under a function as our starting point.  We will adapt this idea in a few specific ways so as to 
@@ -130,13 +134,11 @@ md"We use this canonical image of area under a function as our starting point.  
 2. expand the problem domain of probability distributions and thus statistics."
 
 # ╔═╡ 28dce3a3-63b4-4496-bf99-2779d29881d9
-md"To adapt our understanding of area under a function to the Axioms of Probability, we will require that these distribution-specific functions will need to have area under them equal to 1, and that these functions be non-negative; we don't want probabilities smaller than zero or greater than one.  This ensures that area under the distribution's density function makes an appropriate analogy to probability."
 
-# ╔═╡ 50b6eb2a-e498-4640-b648-43d36d706f14
-md"Further, we need to expand our thinking about area under functions to deal with area under functions defined on countable (or finite) spaces.  The plot below represents area under a function at the points ``x \in \{6, 7, 8, 9\}``, where the function is only defined on the integers between ``0`` and ``10``."
 
 # ╔═╡ 203f0879-875e-4a25-a7df-3c54f72ff41f
 begin
+	cols = get_color_palette(:auto, 17)
 	gaskeypad = (0 => 0.0, 1 => 0.0, 2 => 3/25, 3 => 1/25, 4 => 0.0, 5 => 5/25,
 		6 => 2/25, 7 => 2/25, 8 => 1/25, 9 => 11/25)	
 	y = 0:10
@@ -150,49 +152,29 @@ begin
 	p
 end
 
-# ╔═╡ 19f28e21-8390-4b08-b39d-b5acdd493703
-md"Putting these requirements together we get that a distribution's density function, let's call it ``f``, satisfies ``f(x) \geq 0`` and area under the function be equal to ``1``.  For more details on density functions, see the notebook on [TODO link to] Density Functions.  To generalize area under the (distribution's density) function, for distributions defined on either countable (or finite) spaces or uncountable spaces, we turn to the operation expectation."
+# ╔═╡ 483c1814-c485-4ac2-8940-8c0b3b68de59
+begin
+	x = 1:6
+	xfill = 2:4
+	plot(x, repeat([1/6], 6), t = :scatter,
+		ylimits = (0, 0.25),
+		xlabel = L"x", ylabel = "Density", label = false)
+	plot!(repeat(xfill, 1, 2), repeat([0, 1/6], 3, 2),
+		t = :line, c = cols[1], label = false)
+end
+
 
 # ╔═╡ f54cb70e-b6f1-44f8-b36c-01c6f373fa90
-md"Probability distributions are classified into two classes discrete and continuous.  **Discrete distributions** are probability distributions defined on a finite or countable sample space.  **Continous distributions** are probability distributions defined on an uncountable sample space.  **Expectation** generalizes area under a function to apply to either discrete or continuous distributions.  For more details see the notebook on [TODO add link] Expectation.  For now, we'll re-write Axiom of Probability 2. with an expectation.
 
-$$1 = \mathbb{P}[S] = \mathbb{E}[\mathbb{1}_S]  = \left\{
-        \begin{array}{ll}
-            \sum_{S} \mathbb{1}_S(x) f(x) & \quad \mathbb{P} \text{ is discrete} \\
-            \int_{S} \mathbb{1}_S(x) f(x) & \quad \mathbb{P} \text{ is continuous} \\
-        \end{array}
-    \right.$$"
 
 # ╔═╡ b9c057ef-bff2-479d-a38f-473b7ca4c309
-md"Consider the density function ``f(x)`` from above. Notice that the function is defined to be ``0`` outside of the interval ``[0, 1]``.  We might write this density function's definition as
 
-$$f(x) = x^2 * \mathbb{1}_{[0, 1]}(x).$$
-
-With this definition we can verify the Axioms of Probability.
-
-1. Non-negative probability is assigned to any set ``A \subseteq [0, 1].``
-
-$$\mathbb{P}[A] = \mathbb{E}[\mathbb{1}_A] = \int_{S} \mathbb{1}_A(x) f(x) dx \geq 0$$
-
-2. Probability ``1`` is assigned to the entire sample space.
-
-$$\mathbb{P}[S] = \mathbb{E}[\mathbb{1}_S] = \int_{S} \mathbb{1}_S(x) f(x) dx = 1.$$
-
-3. The probability of a disjoint union of sets is equal to the sum of the probabilities of each of the union's components.
-
-    This follows since sums and intergals, and thus expectation, is a linear operation."
 
 # ╔═╡ f52bc4fd-1a07-4963-8efc-f67152f76152
-md"Alternatively, consider the density function ``g(x)`` from above.  There's not simple way to write this function.  Often in cases like this, a table is used."
+md"Alternatively, consider the density function ``g(x)`` from above.  There's not simple way to write this function, even a piece-wise function would be messy.  Often in cases like this, a table is used."
 
-# ╔═╡ 0d89f93b-3a3a-4c1b-8e63-552ed41c6d4c
-render_table(gkp) = @htl("""
-	<table><caption><h4>Gas Keypad Distribution</h4></caption>
-	<tr><td>Digit<td>Density
-	<tr>$(map(gaskeypad) do @htl("<td>$(string(first(gkp))) for gkp in gaskeypad])</tbody></table>""")
+# ╔═╡ a1b61767-02c5-4678-8cde-3b86cc30bbd7
 
-# ╔═╡ 3ad2e477-ba5b-4676-b8d3-c99fa50b91d5
-# TODO so close! render_table(gaskeypad)
 
 # ╔═╡ d2bca69d-353f-48d9-bde0-677892b3fa24
 md"## Examples"
@@ -217,6 +199,16 @@ md"
 [^1]: Technically probability can only be assigned to measurable sets.  Unfortunately, I will probably not mention this technicality at all times when I should.  TODO see reference instead.
 "
 
+# ╔═╡ 0d89f93b-3a3a-4c1b-8e63-552ed41c6d4c
+render_table(gkp) = @htl("""
+	<table><caption><h4>Gas Keypad Distribution</h4></caption>
+	<tbody>
+	<tr><td>Digit$(map(gaskeypad) do gkp @htl("<td>$(string(first(gkp)))") end)
+	<tr><td>Density$(map(gaskeypad) do gkp @htl("<td>$(string(last(gkp)))") end)</tbody></table>""")
+
+# ╔═╡ 3ad2e477-ba5b-4676-b8d3-c99fa50b91d5
+render_table(gaskeypad)
+
 # ╔═╡ Cell order:
 # ╠═d8ed59fe-a83e-11eb-07f8-d7123a34a1c5
 # ╠═4fd5c54c-4c43-454a-81dd-5ac7245feb85
@@ -239,19 +231,20 @@ md"
 # ╠═8358fab7-4b42-45ec-bcdf-661747dde8f7
 # ╠═9612e871-4b10-4cff-9215-4da903d33d06
 # ╠═59588307-aaa6-45ab-862b-759b9dfc6810
-# ╠═edc9101c-af3b-40f6-b8f3-cf1570f70b9b
-# ╠═e2268550-0ffe-47c2-a23a-e0fa220e0ddb
-# ╠═01fef914-3f03-43fa-aaf2-d9efafeb9c7b
+# ╠═a91d6013-be1a-406d-a193-906e0c8d5a8c
+# ╠═272ce517-d9ec-4afe-b5c1-90d6c4c89899
+# ╠═f10cecb7-cb04-43ee-8db0-f623d99115c1
+# ╠═26cdcadc-f212-4cd2-a159-c85fc1a6574f
+# ╠═483c1814-c485-4ac2-8940-8c0b3b68de59
+# ╠═77a6274a-e7d3-4700-a404-e4c32ec8a29e
 # ╠═6f0da015-f70a-490c-b116-9b9204371f76
 # ╠═28dce3a3-63b4-4496-bf99-2779d29881d9
-# ╠═50b6eb2a-e498-4640-b648-43d36d706f14
 # ╠═203f0879-875e-4a25-a7df-3c54f72ff41f
-# ╠═19f28e21-8390-4b08-b39d-b5acdd493703
 # ╠═f54cb70e-b6f1-44f8-b36c-01c6f373fa90
 # ╠═b9c057ef-bff2-479d-a38f-473b7ca4c309
 # ╠═f52bc4fd-1a07-4963-8efc-f67152f76152
-# ╠═0d89f93b-3a3a-4c1b-8e63-552ed41c6d4c
 # ╠═3ad2e477-ba5b-4676-b8d3-c99fa50b91d5
+# ╠═a1b61767-02c5-4678-8cde-3b86cc30bbd7
 # ╠═d2bca69d-353f-48d9-bde0-677892b3fa24
 # ╠═b284dddb-2da3-4e93-9ecb-b6592dcab087
 # ╠═38f19a20-e002-41b5-976c-22c6af542e89
@@ -259,6 +252,7 @@ md"
 # ╠═e76c82e9-711c-4450-89fe-d795344d2cdb
 # ╠═07a816ec-d1bb-4720-b381-a493643e747d
 # ╠═72b053b8-9327-4128-931e-95b3e9305571
+# ╠═0d89f93b-3a3a-4c1b-8e63-552ed41c6d4c
 # ╠═82eb253a-2378-4074-9dab-4cb3c7eea868
 # ╠═e3f5a522-6051-489e-9f72-89d9ecfc9b2c
 # ╠═bf81ee3b-dd04-4983-9f2e-b52badad1da1
