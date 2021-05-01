0d49afb071e0b1d5cee7d35ad57f769016c64ee5
Edward A. Roualdes <eroualdes@csuchico.edu>
2021-05-01 11:33:55 -0700

    merge all thoughts into this branch; write thoughts down for biasvariancetradeoff notebook

---
+++ notebooks/biasvariance.jl
@@ -0,0 +1,24 @@
+### A Pluto.jl notebook ###
+# v0.14.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 33f8b7a6-aaab-11eb-1a58-6563f29a647d
+# https://www.giulianomega.com/post/2018-06-08-density/#fnref2
+
+# ╔═╡ 67446670-3b9c-4230-bd73-51529f454ee7
+# http://heather.cs.ucdavis.edu/~matloff/256/DensBias.pdf
+# http://heather.cs.ucdavis.edu/~matloff/256/
+
+# ╔═╡ 960cd0fd-841b-4e9e-a86d-3fa8f4d1916b
+md"# Bias Variance Trade-off"
+
+# ╔═╡ 05818f63-31be-4c88-93a8-2589f5d5e9f6
+md"Thnking outloud: Lead with fitting polynomials to showcase bias variance tradeoff, lots of images hopefully interactive (user can add/delete terms with higher powers).  Details in theory as in Hastie's Elements of Statistical Learning, and then follow up histogram example as in links above."
+
+# ╔═╡ Cell order:
+# ╠═33f8b7a6-aaab-11eb-1a58-6563f29a647d
+# ╠═67446670-3b9c-4230-bd73-51529f454ee7
+# ╠═960cd0fd-841b-4e9e-a86d-3fa8f4d1916b
+# ╠═05818f63-31be-4c88-93a8-2589f5d5e9f6
